96a47948e92ed8d95c8a6b03142358ed15b4c238
Kevin Evangelista <kevin.evangelista11@gmail.com>
2019-04-25 23:46:02 -0500

    small update

--- HeroRPG.py
+++ HeroRPG.py
@@ -1,19 +1,24 @@
 class Hero:
-    def __init__(self, health, power):
+    def __init__(self, hero, health, power):
+        self.hero = hero
         self.health = health
         self.power = power
 
     def Attack(self,enemy):
         enemy.health -= self.power
-        print("You do {} damage to the goblin.".format(self.power))
+        print(f"You do {self.power} damage to the goblin.")
         if enemy.health <= 0:
             print("The goblin is dead.")
 
     def Alive(self):
         return self.health > 0
 
+    def PrintStatus(self):
+        print(f"{self.hero} has {self.health} health left")
+
 class Goblin:
-    def __init__(self, health, power):
+    def __init__(self, goblin, health, power):
+        self.goblin = goblin
         self.health = health
         self.power = power
 
@@ -26,24 +31,34 @@ class Goblin:
     def Alive(self):
         return self.health > 0
 
-hero = Hero(10, 5)
-goblin = Goblin(6,2)
+    def PrintStatus(self):
+        print(f"{self.goblin} has {self.health} health left")
+
+hero = Hero('Hero',10, 5)
+goblin = Goblin('Goblin',6,2)
 
 while goblin.health and hero.health > 0:
+    hero.PrintStatus()
+    goblin.PrintStatus()
     print("The goblin has {} health and {} power.".format(goblin.health, goblin.power))
     print("What do you want to do?")
-    print("1. fight goblin")
-    print("2. do nothing")
-    print("3. flee")
+    print("1. Fight Goblin")
+    print("2. Do Nothing")
+    print("3. Flee")
     print("> ", end=' ')
 
     raw_input = input()
 
     if raw_input == "1":
         hero.Attack(goblin)
+        if goblin.health <= 0:
+            print("You have slain your enemy!")
 
     elif raw_input == "2":
         goblin.Attack(hero)
+        if hero.health <= 0:
+            print("You have fallen to a Goblin!")
+
 
     elif raw_input == "3":
         print("You ran like a coward!")
